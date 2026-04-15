9682f65a225d6cafe1d7d63f6c1e217abba1a262
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-15 11:24:55 +0530

    BLC and ELC

---
+++ src/com/BLC/ELC/B.java
@@ -0,0 +1,20 @@
+package com.BLC.ELC;
+
+public class B {
+
+	public static void main(String[] args) {
+		A obj = new A();
+
+		obj.setStName("Gopi");
+		obj.setStCity("HYD");
+		obj.setStId(100);
+		obj.setStMarks(70);
+
+//		System.out.println(obj.getStName());
+//		System.out.println(obj.getStCity());
+//		System.out.println(obj.getStId());
+//		System.out.println(obj.getStMarks());
+          obj.getData();
+	}
+
+}
