0f9301e339ef2027e1549f455806dffd0ef892f1
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-23 11:15:36 +0530

    Wrapper classes

---
+++ src/com/wrapper/Example1.java
@@ -0,0 +1,65 @@
+package com.wrapper;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		//manual Boxing
+		
+//	     int a=30;//integer
+//	     
+//	    Integer i=Integer.valueOf(a);//integer
+//	    System.out.println(i);
+		
+//		byte b=30;
+//		Byte i=Byte.valueOf(b);
+//		System.out.println(i);
+	     
+		//Auto Boxing;
+//		int a=30;
+//		Integer i=a;
+//		System.out.println(i);
+		
+		
+//		int a=130;
+//		int b=130;
+//		
+//		Integer i1=a;//100x
+//		Integer i2=b;//200x
+//		
+//		System.out.println(i1==i2);
+		
+		
+		//Manual Un-boxing
+		
+//		
+//		Integer a=30;
+//		
+//		     int i=a.intValue();
+//		     System.out.println(i);
+		
+//		Byte b=34;
+//		
+//		byte i=b.byteValue();
+//		System.out.println(i);
+		
+		
+		//Auto Un-Boxing
+		
+		
+//		Integer i=30;
+//		
+//		int a=i;
+//		System.out.println(a);
+		
+		
+	
+		
+		
+		
+		
+		
+
+	}
+
+}
